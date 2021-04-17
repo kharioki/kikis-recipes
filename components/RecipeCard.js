b67b0b6503557c588756755500c9a893324e7f0a
kharioki <antolisah@gmail.com>
2021-04-17 08:38:49 +0300

    styling with styled jsx

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -9,8 +9,8 @@ const RecipeCard = ({ recipe }) => {
                 {/* image - thumbnail */}
                 <Image 
                     src={'https:' + thumbnail.fields.file.url}
-                    width={thumbnail.fields.file.details.image.width}
-                    height={thumbnail.fields.file.details.image.height}
+                    width={600}
+                    height={400}
                 />
             </div>
             <div className="content">
@@ -22,6 +22,42 @@ const RecipeCard = ({ recipe }) => {
                     <Link href={'/recipes/' + slug}><a>Cook this</a></Link>
                 </div>
             </div>
+
+            <style jsx>{`
+                .card {
+                    transform: rotateZ(-1deg);
+                }
+                .content {
+                    background: #fff;
+                    box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+                    margin: 0;
+                    position: relative;
+                    top: -40px;
+                    left: -10px;
+                }
+                .info {
+                    padding: 16px;
+                }
+                .info h4 {
+                    margin: 4px 0;
+                    text-transform: uppercase;
+                }
+                .info p {
+                    margin: 0;
+                    color: #777;
+                }
+                .actions {
+                    margin-top: 20px;
+                    display: flex;
+                    justify-content: flex-end;
+                }
+                .actions a {
+                    color: #fff;
+                    background: #f01b29;
+                    padding: 16px 24px;
+                    text-decoration: none;
+                }
+            `}</style>
         </div>
     );
 }
